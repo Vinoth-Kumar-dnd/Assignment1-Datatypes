01871683c08292028d74a3d479ef097a212bde1d
Vinoth-Kumar-dnd <vinothkumarn127@gmail.com>
2025-02-25 16:12:55 +0530

    Update Question6.java

--- Question6.java
+++ Question6.java
@@ -9,13 +9,13 @@ public class Avg_Weight {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int [] weights = new int[10];
+		double [] weights = new double[10];
 		int sum=0;
 		for(int i=0;i<weights.length;i++) {
 			
 			System.out.println("enter the weight of person "+(i+1));
 			
-			weights[i]=sc.nextInt();
+			weights[i]=sc.nextDouble();
 			sum +=weights[i];
 		}
 		double avg=sum/10.0;
